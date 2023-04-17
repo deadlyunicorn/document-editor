4acc275e257ed22ade686813310beb3b9c80810c
deadly-unicorn <retroalex1008@gmail.com>
2023-04-18 01:26:22 +0300

    Can now remo pages.. I hate arrays on react XD.. and ChatGPT.. (but it also did help me (more than StackOverflow))

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,22 @@
 
 import {  ReactNode, createContext, useContext, useEffect, useState } from "react";
 
+interface metaInterface{
+  creatorID:string;
+  setCreatorID:(creatorID:string)=>void;
+  creatorName:string;
+  setCreatorName:(creatorName:string)=>void;
+  docName:string;
+  setDocName:(docName:string)=>void,
+  assignedFor:string,
+  setAssignedFor:(assignedFor:string)=>void,
+  printPrep:boolean,
+  setPrintPrep:(printPrep:boolean)=>void,
+  pageCountToDisplay:number[],
+  setPagesToDisplay:(PagesToDisplay:number[])=>void,
+}
 
-
-const metaContext = createContext({
+const metaContext = createContext<metaInterface>({
   creatorID:"",
   setCreatorID:(creatorID:string)=>{},
   creatorName:"",
@@ -15,12 +28,16 @@ const metaContext = createContext({
   setAssignedFor:(assignedFor:string)=>{},
   printPrep:false,
   setPrintPrep:(printPrep:boolean)=>{},
-  pageCount:1,
-  setPageCount:(oageCount:number)=>{}
-
+  pageCountToDisplay:[],
+  setPagesToDisplay:(PagesToDisplay:number[])=>{}
 });
 
+
+
+
+
 export default function Page() {
+
 
 
 
@@ -29,12 +46,8 @@ export default function Page() {
     e.returnValue="yee"; //works on most browsers.
   };
 
-  useEffect(()=>{
-    window.addEventListener('beforeunload',confirmReload);
-    return()=>{
-      window.removeEventListener('beforeunload',confirmReload);
-    };
-  },[]);
+ 
+  const [pageCountToDisplay,setPagesToDisplay]=useState<number[]>([]);
   
   const [docName,setDocName]=useState<string>("");
   const [assignedFor,setAssignedFor]=useState<string>("");
@@ -42,9 +55,20 @@ export default function Page() {
   const [creatorName,setCreatorName]=useState<string>("");
   const [creatorID,setCreatorID]=useState<string>("");
 
-  const [pageCount,setPageCount] = useState(1);
 
   const [printPrep,setPrintPrep]=useState<boolean>(false);
+
+
+
+
+
+
+  useEffect(()=>{
+    window.addEventListener('beforeunload',confirmReload);
+    return()=>{
+      window.removeEventListener('beforeunload',confirmReload);
+    };
+  },[]);
 
   
   const value = {
@@ -58,9 +82,11 @@ export default function Page() {
     setAssignedFor:setAssignedFor,
     printPrep:printPrep,
     setPrintPrep:setPrintPrep,
-    pageCount:pageCount,
-    setPageCount:setPageCount
+    pageCountToDisplay:pageCountToDisplay,
+    setPagesToDisplay:setPagesToDisplay,
   };
+
+ 
 
 
 
@@ -69,7 +95,7 @@ export default function Page() {
     
   
 
-    <main className="data-[colorDefault=true]:bg-[#ffe4c4]" data-colorDefault={printPrep}>
+    <main className="data-[color_default=true]:bg-[#ffe4c4]" data-color_default={printPrep}>
       <metaContext.Provider value={value}>
         <PagesSet/>
         <OptionMenu/>
@@ -83,24 +109,44 @@ export default function Page() {
 const PagesSet=()=>{
 
   ///add a confirm box when deleting pages..
-  const PagesToDisplay:ReactNode[] = [];
-  const {setPageCount,pageCount,printPrep}=useContext(metaContext);
+  const {pageCountToDisplay,setPagesToDisplay,printPrep}=useContext(metaContext);
+
   
 
+    const removePage=(key:number)=>{
+      const newPagesToDisplay = pageCountToDisplay.filter((page,index)=>index!==key);
+      setPagesToDisplay([...newPagesToDisplay]); //spread operator needed for some reason
+    }
+  
 
+    useEffect(()=>{
+    },[pageCountToDisplay]);
 
-  for(let i=0;i<pageCount;i++){
-    PagesToDisplay.push(
-      <div key={i}>
-        {!printPrep&&pageCount>1&&<button className="absolute" onClick={()=>{PagesToDisplay.splice(i,1);setPageCount(pageCount-1)}}>Remove Page</button>}
-        <PageComponent pageNum={i+1}/>
-      </div>
-    )
-  }
 
   return(
     <div>
-      {PagesToDisplay}
+      {pageCountToDisplay.map((pageNum:number,pageIndex:number)=>(
+        <div key={pageIndex}>
+        {!printPrep&&pageCountToDisplay.length>1&&
+
+        <button 
+          className="absolute"
+          onClick={()=>{
+            
+              removePage(pageIndex)
+
+              //setPagesToDisplay(PagesToDisplay.filter((page,i)=>{alert(i);return i!=index})) 
+              //at the beginning I used filter() that removed said the page using its index. Then the rest of the items were getting their indexes removed by 1 so that made it remove the rest, as well.. 
+              //this gets the value at the time of pressing the button, not when it was rendered.
+              //index above reaches until the page you want to remove then stops 
+              //find a way to get the div key of the div of the component. Maybe with document.querySelector()->pageNum?
+              }}>
+                Remove Page {pageIndex+1}
+                
+        </button>}
+        <PageComponent pageNum={pageIndex+1}/>
+      </div>
+      ))}
     </div>
   )
 }
@@ -123,98 +169,26 @@ const PageComponent = (
 
   return(
 
-  <div className="A4 data-[printCheck]" data-printCheck={printPrep}>
+  <div className="A4 data-[print_check]" data-print_check={printPrep}>
 
-    <input className="h1" placeholder="Page Heading">
-    </input>
-    <DocumentInfo/>
- 
+    <div className="gap-10 flex flex-col">
+      <input className="h1" placeholder="Page Heading"/>
+      <DocumentInfo/>
       <MainContent>
-        {sampleText}
+        <textarea placeholder="hello" className=" w-full"/>
       </MainContent>
+    </div>
 
-    <CreatorInfo/>
-    {pageNum}
+    <div>
+      <CreatorInfo/>
+      {pageNum}
+    </div>
+
   </div>
   );
 }
 
-  {/* <h1>Θέμα Β</h1>
-  <div class="display: flex; justify-content: space-around; width: 100%;">
-    <div>
-      Βάσεις Δεδομένων Θεωρία
-    </div>
-    <div>
-      Εργασία B' Εξαμήνου
-    </div>
-  </div>
-  <div class="div-center-content">
-    
-    <h2 class="text-align: center;"><u>Δημιουργία πίνακα με DDL</u></h2>
-    <p class="border-bottom: solid black 1px; padding-bottom: 5px;">
-      <br/>
-      <i class="italicColor">CREATE TABLE</i> ΣΧΟΛΕΙΟ(
-        <br/>ΚΩΔΙΚΟΣ <i class="italicColor">VARCHAR(6)</i> <b><span class="color: blueviolet;">PRIMARY KEY</span></b>,
-        <br/>ΟΝΟΜΑ <i class="italicColor">VARCHAR(25)</i>,
-        <br/>ΔΙΕΥΘΥΝΣΗ <i class="italicColor">VARCHAR(40)</i>,
-        <br/>ΑΡ_ΜΑΘΗΤΩΝ <i class="italicColor">INT</i>
-        <br/>);
-    </p>
-    
-    <br/>
-    <h2 class="text-align: center;"><u>Εισαγωγή τιμών με DML</u></h2>
-    <div class="display: grid; grid-template-columns:repeat(2,1fr); gap: 40px;"> 
-
-      <p class="border-bottom: solid black 1px; padding-bottom: 5px;">
-        <br/>
-        <i class="italicColor">INSERT INTO</i> ΣΧΟΛΕΙΟ <i class="italicColor">VALUES</i>(
-          <br/><span class="seaGreen">'0Α1420'</span>,
-          <br/><span class="seaGreen">'9ο ΓΕ.Λ. ΑΘΗΝΩΝ'</span>,
-          <br/><span class="seaGreen">'ΑΘΗΝΑ'</span>,
-          <br/><i class="italicColor">245</i>
-        <br/>);
-      </p>
-    
-
-      <p class="border-bottom: solid black 1px; padding-bottom: 5px;">
-      <br/>
-      <i class="italicColor">INSERT INTO</i> ΣΧΟΛΕΙΟ <i class="italicColor">VALUES</i>(
-        <br/><span class="seaGreen">'0Σ0420'</span>,
-        <br/><span class="seaGreen">'2ο ΓΕ.Λ. ΠΕΙΡΑΙΑ'</span>,
-        <br/><span class="seaGreen">'ΠΕΙΡΑΙΑΣ'</span>,
-        <br/><i class="italicColor">311</i> 
-      <br/>);
-      </p>
-
-      <p class="border-bottom: solid black 1px; padding-bottom: 5px;">
-        <i class="italicColor">INSERT INTO</i> ΣΧΟΛΕΙΟ <i class="italicColor">VALUES</i>(
-          <br/><span class="seaGreen">'0Β0111'</span>,
-          <br/><span class="seaGreen">'2ο ΕΠΑ.Λ. ΑΘΗΝΑΣ'</span>,
-          <br/><span class="seaGreen">'ΑΘΗΝΑ'</span>,
-          <br/><i class="italicColor">186</i> 
-        <br/>);
-      </p>
-
-      <p class="border-bottom: solid black 1px; padding-bottom: 5px;">
-        <i class="italicColor">INSERT INTO</i> ΣΧΟΛΕΙΟ <i class="italicColor">VALUES</i>(
-          <br/><span class="seaGreen">'0ΒΙ020'</span>,
-          <br/><span class="seaGreen">'1ο ΓΕ.Λ. ΚΟΜΟΤΗΝΗΣ'</span>,
-          <br/><span class="seaGreen">'ΚΟΜΟΤΗΝΗ'</span>,
-          <br/><i class="italicColor">112</i> 
-        <br/>);
-      </p>
-    </div>
-    
-  </div>
-  <div class="display: flex; justify-content: space-around; width: 100%;">
-    <div>
-      Αλέξανδρος Πετράκε
-    </div>
-    <div>
-      tep8677
-    </div>
-  </div> */}
-
+ 
 
   const MainContent = ({children}:{children:ReactNode}) => {
     return(
@@ -242,6 +216,9 @@ const PageComponent = (
     )
   }
   
+
+
+
   const CreatorInfo=()=>{
 
     const {creatorName,setCreatorName,creatorID,setCreatorID}=useContext(metaContext);
@@ -261,34 +238,26 @@ const PageComponent = (
 
   const OptionMenu=()=>{
     const {printPrep,setPrintPrep}=useContext(metaContext);
-    const {setPageCount,pageCount}=useContext(metaContext);
+
+    const {pageCountToDisplay,setPagesToDisplay}=useContext(metaContext);
 
     
     return(
-      <form className="h-fit grid-cols-2 grid A4 text-blue-500 data-[printCheck]" data-printCheck={printPrep}>
+      <div className="h-fit grid-cols-2 grid A4 text-blue-500 data-[print_check]" data-print_check={printPrep}>
         <label>
           Print Show
           <input id="print" type="checkbox" onChange={()=>{setPrintPrep(!printPrep);}}/>
         </label>
 
-        <label>
-          Pages to Display
-          <input id="pageCount" type="number" value={pageCount} onChange={(event)=>{
-            const pageCount=+event.target.value;
-            if(pageCount<=12&&pageCount>0){
-              setPageCount(pageCount);}
-            else if(pageCount>12){setPageCount(12)}
-            }} min={1} max={12}/>
-        </label>
-        
-        {printPrep+""}
-        <span>h</span>
-        <span>h</span>
-        <span>h</span>
-        <span>h</span>
-        <span>h</span>
+        <button  onClick={()=>{
+              pageCountToDisplay.length<12&&
+              setPagesToDisplay([...pageCountToDisplay,pageCountToDisplay.length+1])
+            }}>
+          Add Page
+        </button>
+      
 
-      </form>
+      </div>
 
     )
   }
