3ecc61d096bd07795db3f7de9cd01c9ec2fdc521
deadly-unicorn <retroalex1008@gmail.com>
2023-04-27 03:23:04 +0300

    reorganized some code

---
+++ app/components/colorContext.ts
@@ -0,0 +1,31 @@
+import { createContext } from "react";
+
+export interface colorInterface{
+  bg1:string,
+  setBg1:(color:string)=>void,
+  bg2:string,
+  setBg2:(color:string)=>void,
+  textColor:string,
+  setTextColor:(color:string)=>void,
+  headingColor:string,
+  setHeadingColor:(color:string)=>void,
+  headingColor2:string,
+  setHeadingColor2:(color:string)=>void,
+  pageBg:string,
+  setPageBg:(color:string)=>void,
+};
+
+export const colorContext = createContext<colorInterface>({
+  bg1:"#00000",
+  setBg1:(color:string)=>{},
+  bg2:"#00000",
+  setBg2:(color:string)=>{},
+  textColor:"#00000",
+  setTextColor:(color:string)=>{},
+  headingColor:"#00000",
+  setHeadingColor:(color:string)=>{},
+  headingColor2:"#00000",
+  setHeadingColor2:(color:string)=>{},
+  pageBg:"#00000",
+  setPageBg:(color:string)=>{},
+});
---
+++ app/components/metaContext.ts
@@ -0,0 +1,38 @@
+import { createContext } from 'react';
+import { userInputArray } from '../page';
+
+
+export const metaContext = createContext<metaInterface>({
+  creatorID:"",
+  setCreatorID:(creatorID:string)=>{},
+  creatorName:"",
+  setCreatorName:(creatorName:string)=>{},
+  docName:"",
+  setDocName:(docName:string)=>{},
+  assignedFor:"",
+  setAssignedFor:(assignedFor:string)=>{},
+  printPrep:false,
+  setPrintPrep:(printPrep:boolean)=>{},
+  pagesToDisplay:[],
+  setPagesToDisplay:(PagesToDisplay:userInputArray)=>{},
+  characterCount:0,
+  setCharacterCount:(num:number)=>{}
+
+});
+
+export interface metaInterface{
+  creatorID:string;
+  setCreatorID:(creatorID:string)=>void;
+  creatorName:string;
+  setCreatorName:(creatorName:string)=>void;
+  docName:string;
+  setDocName:(docName:string)=>void,
+  assignedFor:string,
+  setAssignedFor:(assignedFor:string)=>void,
+  printPrep:boolean,
+  setPrintPrep:(printPrep:boolean)=>void,
+  pagesToDisplay:userInputArray,
+  setPagesToDisplay:(PagesToDisplay:userInputArray)=>void,
+  characterCount:number,
+  setCharacterCount:(num:number)=>void,
+}
